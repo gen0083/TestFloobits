bef90e17fed8f933184ee449d0f73bf9ee143e43
Gen <archiherewego@gmail.com>
2018-09-14 16:44:13 +0900

    Test floobits

--- app/src/main/java/jp/gcreate/testfloobits/MainActivity.java
+++ app/src/main/java/jp/gcreate/testfloobits/MainActivity.java
@@ -2,6 +2,7 @@ package jp.gcreate.testfloobits;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("test", "test hoge");
+        Log.i("test", "hogehoge");
+
+        // abc
+        // abc
+        // abc hoge
+
     }
 }
